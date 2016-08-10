19a1890faf65161a59b0351034dcd228fd17bcdc
Anton Krylov <anthony.kryloff@gmail.com>
2016-08-10 19:14:02 +0500

    improved models, added urls, some views and template stubs

--- exams_scheduler/models.py
+++ exams_scheduler/models.py
@@ -8,11 +8,18 @@ from django.db import models
 class Group(models.Model):
     name = models.CharField(max_length=20)
 
+    def __unicode__(self):
+        return self.name
+
 
 class Professor(models.Model):
     name = models.CharField(max_length=50)
+    name_short = models.CharField(max_length=16)
     subject_longname = models.CharField(max_length=50)
     subject_shortname = models.CharField(max_length=15)
+
+    def __unicode__(self):
+        return self.name_short
 
 
 class Exam(models.Model):
@@ -20,9 +27,16 @@ class Exam(models.Model):
     group = models.ForeignKey(Group)
     professor = models.ForeignKey(Professor)
 
+    def __unicode__(self):
+        return "%s of %s" % (self.professor.subject_shortname, self.group)
+
 
 class ProfessorDay(models.Model):
     date = models.DateField()
+    professor = models.ForeignKey(Professor)
     available = models.BooleanField(default=True)
-    time = models.TimeField()
-    
+    time = models.TimeField(null=True)
+    comment = models.TextField(null=True)
+
+    def __unicode__(self):
+        return "%s @ %s" % (self.professor, self.date)
--- exams_scheduler/urls.py
+++ exams_scheduler/urls.py
@@ -1,3 +1,19 @@
-urlpatterns = [
+from django.conf.urls import url, include
+from django.views.generic import TemplateView
+from .views import *
 
+app_name = 'exams_scheduler'
+
+urlpatterns = [
+    url(r'^student/', include([
+        url(r'^$', TemplateView.as_view(template_name='exams_scheduler/student_index.html'), name='student_index'),
+        url(r'^(?P<professor_id>[0-9]+)/$', student_detail, name='student_detail'),
+        url(r'^add/$', student_add),
+        url(r'^delete/$', student_delete)
+    ])),
+
+    url(r'^professor/', include([
+        url(r'^$', professor_index, name='professor_index'),
+        url(r'^(?P<day>[0-9]{2})-(?P<month>[0-9]{2})/$', ProfessorDayView.as_view(), name='professor_day')
+    ]))
 ]
